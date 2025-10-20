4ffb8068a10191855336a3ae876d9ac4d662c8fb
Philip Krück <hi@philipkrueck.com>
2025-10-20 12:51:43 +0100

    feat: add learnGoWithTests arrays chapters

--- golang/learnGoWithTests/arrays/arrays.go
+++ golang/learnGoWithTests/arrays/arrays.go
@@ -1,8 +1,29 @@
 package arrays
 
-func sum(inp []int) (sum int) {
-	for _, num := range inp {
+func Sum(numbers []int) (sum int) {
+	for _, num := range numbers {
 		sum += num
 	}
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+func SumAllTails(tailsToSum ...[]int) (sums []int) {
+	for _, numbers := range tailsToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
 	return
 }
